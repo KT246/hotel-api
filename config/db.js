cb5ecde0776e31b11a77a2b3df38ef360a487d37
KT246 <tayattapeu@gmail.com>
2024-11-02 19:56:28 +0700

    fsfsFsfertwvzxcf

--- config/db.js
+++ config/db.js
@@ -1,11 +1,9 @@
 const mongoose = require("mongoose");
-
+require("dotenv").config();
 const connectedDB = async () => {
+  const URI = process.env.MONGODB_URI;
   try {
-    await mongoose.connect(
-      process.env.MONGODB_URI ||
-        "mongodb+srv://root-api:fwftpmlmwklmfkewop3opkop@hotel-api.1rfqe.mongodb.net/?retryWrites=true&w=majority&appName=hotel-api"
-    );
+    await mongoose.connect(URI);
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("Error connecting to the database:", error);
