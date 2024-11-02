888f75e8ca6d4704031ce493b9de3edb16f988c9
KT246 <tayattapeu@gmail.com>
2024-11-02 19:33:04 +0700

    four commit

--- config/db.js
+++ config/db.js
@@ -3,10 +3,7 @@ const mongoose = require("mongoose");
 
 const connectedDB = async () => {
   try {
-    await mongoose.connect(process.env.DATABASE_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.DATABASE_URL);
     console.log("Connected...!");
   } catch (error) {
     console.log("Error connecting to the database:", error);
