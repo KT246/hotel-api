186f5c22fe972ec902f9fac79dc53ddeec4cb3b6
KT246 <tayattapeu@gmail.com>
2024-11-02 19:51:17 +0700

    fsf

--- config/db.js
+++ config/db.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 
 const connectedDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(
+      process.env.MONGODB_URI ||
+        "mongodb+srv://root-api:fwftpmlmwklmfkewop3opkop@hotel-api.1rfqe.mongodb.net/?retryWrites=true&w=majority&appName=hotel-api"
+    );
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("Error connecting to the database:", error);
