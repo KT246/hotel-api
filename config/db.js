5833dd52f6c6c5a3e7cdd5731c34384fb915fabb
KT246 <tayattapeu@gmail.com>
2024-11-02 20:13:32 +0700

    sfsdfsdferw3

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
-require("dotenv").config();
 const mongoose = require("mongoose");
+require("dotenv").config();
 const connectedDB = async () => {
   const URI = process.env.MONGODB_URI;
   try {
     await mongoose.connect(URI);
-    console.log("Connected to MongoDB");
+    console.log("Connected to MongoDB!");
   } catch (error) {
     console.error("Error connecting to the database:", error);
   }
